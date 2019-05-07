aab77a7ad96b906dcb4ab47e0a15240ef9d6ebda
Bente Gber <benjamingreenberg@Benjamins-MacBook-Pro-2.local>
2019-05-06 20:38:24 -0400

    added backend message GET and PUT as well as User POST

--- ChatApp/Backend/backend.js
+++ ChatApp/Backend/backend.js
@@ -12,8 +12,6 @@ export default {
     },
 
 
-
-
     getAllMessages: function (eventID) {
         return fetch(
             `http://localhost:3001/event/message/${eventID}`,
@@ -22,5 +20,17 @@ export default {
                 headers: { 'Content-Type': 'application/json' },
             },
         )
+    },
+
+    createUserId: function ( user ) {
+        return fetch(`http://localhost:3001/user`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(user),
+        });
     }
+
+//     getEvent: function (){
+//         return fetch(`http://localhost:3001/event`);
+//     }
 }
--- ChatApp/src/Components/chat.js
+++ ChatApp/src/Components/chat.js
@@ -1,100 +1,120 @@
 import React from 'react';
 
 import { GiftedChat } from 'react-native-gifted-chat';
-import Backend from '../../Backend/backend'
+import Backend from '../../Backend/backend';
 
 class Chat extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            _id: 1,
-            user: '5ccdf852720df265a0bbcb39',
-            name: this.props.data,
-            message: '',
-            messages: []
-        }
-
-    }
-    // state = {
-    //     user: '',
-    //     name: '',
-    //     message: '',
-    //     messages: []
-    // }
-
-
-    componentWillMount() {
-
-    }
-
-    componentDidMount() {
-        console.log('State before messages', this.state)
-        const testID = '5cc88bf7358cd52db165aa57';
-        Backend.getAllMessages(testID)
-            .then((response) => response.json())
-            .then((data) => {
-                this.setState((currentState) => {
-                    console.log('This: ', data)
-                    return {
-                        messages: GiftedChat.append(currentState, data)
-                    }
-                }, () => {
-                    console.log('State after messages: ', this.state)
-                })
-
-            });
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      _id: 1 /*This is the User's ID that we will need to get from the login flow*/,
+      user: {
+        _id: '5ccdf852720df265a0bbcb39',
+        name: this.props.data,
+      },
+      message: '',
+      messages: [],
+      eventID: '5ccb7d48c3c78ecef4a8f5dd',
     };
-    // componentWillMount() {
-    //     Backend.closeChat()
-    // }
-    handleSend(message) {
-        //send message to backend
-        Backend.addNewMessage({
-            name: this.state.name,
+  }
+  // state = {
+  //     user: '',
+  //     name: '',
+  //     message: '',
+  //     messages: []
+  // }
+
+  componentWillMount() {}
+
+  componentDidMount() {
+    const testID = '5ccb7d48c3c78ecef4a8f5dd';
+    console.log('State before messages', this.state);
+    Backend.getAllMessages(testID)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState(
+          (currentState) => {
+            console.log('This line 35: ', data);
+            return {
+              messages: GiftedChat.append(currentState, data),
+            };
+          },
+          () => {
+            Backend.getAllMessages(testID);
+            console.log('State after messages: ', this.state);
+          },
+        );
+      });
+  }
+  // componentWillMount() {
+  //     Backend.closeChat()
+  // }
+  handleSend(message) {
+    //send message to backend
+    console.log('here');
+    Backend.addNewMessage(message, this.state.eventID)
+      .then((response) => {
+        response.json();
+        console.log(response);
+      })
+      .then((data) => {
+        this.setState(
+          (currentState) => {
+            console.log('This line 66: ', data);
+            return {
+              messages: GiftedChat.append(currentState, data),
+            };
+          },
+          () => {
+            console.log('State after messages sent: ', this.state);
+          },
+        );
+      });
+    console.log('Message line 75: ', message);
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={(message) => {
+          //send message to backend
+          Backend.addNewMessage(
             message,
-        }, this.state.eventID)
-            .then(response => {
-                response.json()
-                console.log(response)
-            })
-            .then((data) => {
-                this.setState((currentState) => {
-                    console.log('This line 66: ', data)
-                    return {
-                        messages: GiftedChat.append(currentState, data)
-                    }
-                }, () => {
-                    console.log('State after messages sent: ', this.state)
-                })
 
-            });
-        console.log(message)
-    }
-
-    render() {
-        return (
-            <GiftedChat
-                messages={this.state.messages}
-                onSend={(message) => {
-                    //send message to backend
-                    Backend.addNewMessage({
-                        name: this.state.name,
-                        message,
-                    }, '5ccb7d48c3c78ecef4a8f5dd')
-                    console.log(message)
-                }}
-                user={{
-                    _id: this.state._id,
-                    name: this.state.name
-                }}
-            />
-        )
-    }
-
-
+            this.state.eventID,
+          )
+            .then((response) => response.json())
+            .then((data) =>{this.setState(
+                              (currentState) => {
+                                console.log(
+                                  'This line 66: ',
+                                  data,
+                                );
+                                return {
+                                  messages: GiftedChat.append(
+                                    currentState,
+                                    data,
+                                  ),
+                                };
+                              },
+                              () => {
+                                console.log(
+                                  'State after messages sent: ',
+                                  this.state,
+                                );
+                              },
+                            );});
+          console.log(message);
+        }}
+        user={{
+          _id: this.state.user._id,
+          name: this.state.user.name,
+        }}
+      />
+    );
+  }
 }
-
 
 // Chat.defaultProps = {
 //     name: '',
